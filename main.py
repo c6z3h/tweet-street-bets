151a57afef5e97381b449d883eb1dc52d36f4d68
ZhiHong Chua <chuazhihong@hotmail.com>
2021-05-18 23:01:59 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Documentation:
+1. Tweepy - https://docs.tweepy.org/en/latest/
+2. PyFinance - https://pypi.org/project/pyfinance/
+3. Natural Language Tool Kit - https://www.nltk.org/
+"""
+
+# pip install tweepy
+# pip install pyfinance
+# pip install nltk
+
+# Function 1: Extract Tweets from Interesting Person (eg. Elon Musk / Mark Cuban)
+
+# Function 2: Extract Stock Market Data from PyFinance
+
+# Function 3: Use NLTK to Evaluate the Tweets - Stock Market Link
